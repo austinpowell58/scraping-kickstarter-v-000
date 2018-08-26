4d7f407f4356341dbaad75a9d56c2d17874ca2c3
Austin Powell <austinpowell58@gmail.com>
2018-08-26 17:31:53 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,8 @@ html = File.read('fixtures/kickstarter.html')
 kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
-  #projects : kickstarter.css("li.project.grid_4").first
+  # projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
   binding.pry
 end
